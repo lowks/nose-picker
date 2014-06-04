531176ff7c7b87e5edd808b2d9b4e5ffa824bfd9
Eyal Reuveni <eyal@eventbrite.com>
2014-06-04 08:53:33 -0700

    Change README.md to README.rst in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 VERSION = '0.5'
 
 
